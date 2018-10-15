cfb8da4deb21aa76813f76bb429087e7d9566fbb
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-10-14 18:08:14 -0700

    reorganize + continue implementing infer code.

--- yasit/infer.py
+++ yasit/infer.py
@@ -1,7 +1,36 @@
 import operator as op
+from itertools import combinations
 
 import attr
 import funcy as fn
+
+
+def _info_gain(p, q):
+    return p*(np.log(q) - np.log(q))
+
+
+def info_gain(p, q):
+    if p == q:
+        return 0
+    elif p == 0:
+        return _info_gain(1, 1 - q)
+    elif p == 1: 
+        return _info_gain(1, q)
+    else:
+        return _info_gain(p, q) + _info_gain(1 - p, 1 - q)
+
+
+def score(data, rand):
+    worse_than_random = data < rand
+    if 0 if worse_than_random else info_gain(data, rand)
+
+
+def smallest(concept_class):
+    return reduce(op.and_, concept_class)
+
+
+def contain_trc(concept_class, trc):
+    return (spec for spec in concept_class if spec(trc))
 
 
 @attr.s(auto_attribs=True)
@@ -9,7 +38,7 @@ class State:
     rand_sat_oracle: "Oracle"
     ndemos: int
     best_spec: "Spec" = None
-    best_score: float = 0
+    best_score: float = -float('inf')
     lower_bound: float = 0
     next_lower_bound: float = 0
     nsat: int = -1
@@ -30,56 +59,20 @@ class State:
             self.nsat = nsat
             self.lower_bound, self.next_lower_bound = self.next_lower_bound, 1
 
-
-def _info_gain(p, q):
-    return p*(np.log(q) - np.log(q))
-
-
-def info_gain(p, q):
-    if p == q:
-        return 0
-    elif p == 0:
-        return _info_gain(1, 1 - q)
-    elif p == 1: 
-        return _info_gain(1, q)
-    else:
-        return _info_gain(p, q) + _info_gain(1 - p, 1 - q)
+    def prune(self):
+        return False
 
 
-def score(data, rand):
-    if data < rand:
-        return 0
-
-    return info_gain(data, rand)
-
-
-def smallest(concept_class):
-    return reduce(op.and_, concept_class)
-
-
-def contain_trc(concept_class, trc):
-    return (spec for spec in concept_class if spec(trc))
-
-
-def prune(nsat, low, best_score):
-    return False
-
-
-def score_frontier(n_sat, ):
-    pass
-
-
-
-@lru_cache()
-def rand_sat():
-    pass
-
-
-def infer(demos, basis, n_sat, rand_sat):
+def infer(demos, basis, n_sat, rand_sat_oracle):
+    state = State(rand_sat_oracle, len(demos))
     visited = {smallest(contain_trc(trc)) for trc in demos}
-
-    state = State(rand_sat, len(demos))
     for nsat in range(len(demos)):
         state.update_nsat(nsat)
-        if prune(nsat, low, best_score):
-            continue
+        for trcs in combinations(demos, nsat):
+            if state.prune():
+                break
+            trcs_basis = reduce(op.or_, (visited[trc] for trc in trcs))
+            spec = smallest(trcs_basis)
+            state.update(spec, nsat)
+
+    return state.best_spec
